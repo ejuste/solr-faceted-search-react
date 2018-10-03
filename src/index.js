9eb758b9acb51f8034822885ff68018f8a58ff3a
Emmanuel Juste <emmanuel.juste@ullink.com>
2018-10-03 13:38:32 +0200

    Make the project a lib
    change the source code from src to src/lib
    use yarn instead of npm
    upgrade some dependencies
    add public folder and index.html, add index.js
    to get a simple app to dev/test the lib
    
    Change-Id: Ia7e8f85366534245f55c4bdf2d48160f93331457

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
-import SolrFacetedSearch from "./components/solr-faceted-search";
-import defaultComponentPack from "./components/component-pack";
-import {SolrClient} from "./api/solr-client";
+import React from "react";
+import ReactDOM from "react-dom";
+import { SolrFacetedSearch, SolrClient } from "./lib/";
 
-export default SolrFacetedSearch;
+// The search fields and filterable facets you want
+const fields = [
+  { label: "All text fields", field: "*", type: "text" },
+  { label: "Name", field: "name_t", type: "text" },
+  { label: "Characteristics", field: "characteristics_ss", type: "list-facet" },
+  { label: "Date of birth", field: "birthDate_i", type: "range-facet" },
+  { label: "Date of death", field: "deathDate_i", type: "range-facet" }
+];
 
-export {
-  SolrFacetedSearch,
-  defaultComponentPack,
-  SolrClient
-};
+// The sortable fields you want
+const sortFields = [
+  { label: "Name", field: "koppelnaam_s" },
+  { label: "Date of birth", field: "birthDate_i" },
+  { label: "Date of death", field: "deathDate_i" }
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+  // The client class
+  new SolrClient({
+    // The solr index url to be queried by the client
+    url: "http://localhost:8983/solr/gettingstarted/select",
+    searchFields: fields,
+    sortFields: sortFields,
+
+    // The change handler passes the current query- and result state for render
+    // as well as the default handlers for interaction with the search component
+    onChange: (state, handlers) =>
+      // Render the faceted search component
+      ReactDOM.render(
+        <SolrFacetedSearch
+          {...state}
+          {...handlers}
+          bootstrapCss={true}
+          onSelectDoc={doc => console.log(doc)}
+        />,
+        document.getElementById("root")
+      )
+  }).initialize(); // this will send an initial search, fetching all results from solr
+});
