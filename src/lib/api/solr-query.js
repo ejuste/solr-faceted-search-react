7b45a47152f7612ab481fac9c85f828c6a5372bf
Francois Hervieux <francois.hervieux@itiviti.com>
2018-12-28 17:32:09 +0100

    Wrap text query filter in parenthesis

--- src/lib/api/solr-query.js
+++ src/lib/api/solr-query.js
@@ -36,7 +36,7 @@ const textFieldToQueryFilter = (field) => {
     return null;
   }
 
-  return encodeURIComponent(field.field === "*" ? value : `${field.field}:${value}`);
+  return encodeURIComponent(field.field === "*" ? value : `${field.field}:(${value})`);
 };
 
 const fieldToQueryFilter = (field) => {
